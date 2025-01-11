2fe6457e462abdb0eb806a6b268b4e2fce10681d
Guillermo Gutierrez <guillermo@phosforus.io>
2025-01-11 15:58:58 -0500

    Overengineer dev setup

--- bin/setup-dev.sh
+++ bin/setup-dev.sh
@@ -2,35 +2,51 @@
 
 set -e
 
+REMAKE_ENV=${REMAKE_ENV:-true}
+SKIP_AWS=${SKIP_AWS:-false}
 SKIP_PYTHON=${SKIP_PYTHON:-false}
+SKIP_POSTGRES=${SKIP_POSTGRES:-false}
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 echo "Starting dev setup from ${SCRIPT_DIR}"
 pushd "${SCRIPT_DIR}" &> /dev/null
 
+# Begin .env setup
+TOM_ROOT=$( cd "${SCRIPT_DIR}/.." && pwd )
+ENV_FILE="${TOM_ROOT}/.env"
+
 # Setup AWS CLI or prompt user to install it
-source ./setup-aws.sh
+if [[ "${SKIP_AWS}" == "true" ]]; then
+  echo "Skipping AWS setup"
+else
+  source ./setup-aws.sh
+fi
 
 # Setup Pyenv and install Python version for TOM
 if [[ "${SKIP_PYTHON}" == "true" ]]; then
-  echo "Skipping Python setup."
+  echo "Skipping Python setup"
 else
   source ./setup-python.sh
 fi
 
 # Install Postgresql, run local database, and setup dev user
-echo "Setting up local PostgreSQL database..."
-POSTGRES_LOGIN=$( source ./setup-postgres.sh )
-POSTGRES_USER=$( echo "${POSTGRES_LOGIN}" | cut -d ":" -f 1)
-POSTGRES_PASSWORD=$( echo "${POSTGRES_LOGIN}" | cut -d ":" -f 2)
+if [[ "${SKIP_POSTGRES}" == "true" ]]; then
+  echo "Skipping Postgres setup"
+else
+  POSTGRES_USER=${POSTGRES_USER:-dev}
+  POSTGRES_PASSWORD=${POSTGRES_PASSWORD:-default}
+  POSTGRES_VERSION=${POSTGRES_VERSION:-14}
+  POSTGRES_CLUSTER=${POSTGRES_CLUSTER:-dev}
+  source ./setup-postgres.sh "${POSTGRES_USER}" "${POSTGRES_PASSWORD}" "${POSTGRES_VERSION}" "${POSTGRES_CLUSTER}"
+fi
 
-# Setup .env
-TOM_ROOT=$( cd "${SCRIPT_DIR}/.." && pwd )
-echo "# Base environment setup" > "${TOM_ROOT}/.env"
-{
-  echo "TOM_ROOT=${TOM_ROOT}"
-  echo "AWS_PROFILE=dev"
-  echo "PYTHON_VERSION=$( python --version | sed 's/Python //g' )"
-  echo "PRISMA_SCHEMA_PATH=${TOM_ROOT}/tom-ui/prisma/schema.prisma"
-  echo "DATABASE_URL=postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@localhost"
-} >> "${TOM_ROOT}/.env"
+if [ "${REMAKE_ENV}" == "true" ]; then
+  echo "# Base environment setup" > "${ENV_FILE}"
+  {
+    echo "TOM_ROOT=${TOM_ROOT}"
+    echo "AWS_PROFILE=dev"
+    echo "PYTHON_VERSION=$( python --version | sed 's/Python //g' )"
+    echo "PRISMA_SCHEMA_PATH=${TOM_ROOT}/tom-ui/prisma/schema.prisma"
+    echo "DATABASE_URL=postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@localhost"
+  } >> "${ENV_FILE}"
+fi
